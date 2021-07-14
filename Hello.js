0db6bf27517d28427393916a073975951dbc1594
NaineshKanani <87408081+NaineshKanani@users.noreply.github.com>
2021-07-14 17:04:45 +0530

    Day-1 Assignment

---
+++ Hello.js
@@ -0,0 +1,17 @@
+import React from "react"
+// import ReactDOM from "react-dom"
+import "./style.css"
+
+function Hello(){
+    return(
+        <div>
+            <center>
+            <h1>Hello World</h1>
+            <h1>Hello LetsUpgrade</h1>
+            </center>
+        </div>
+    )
+
+}
+
+export default Hello
